ad47748b55165b7d00932390b27dcf01963957f2
yipu0v0 <firstawhois@gmail.com>
2021-11-20 18:36:37 +0800

    Comment natspec in DAORoom.sol.

--- contracts/DAORoom.sol
+++ contracts/DAORoom.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+/// @title A simulator of one room, pay fee to occupy it!
+/// @notice Pay fee to occupy a room!
 contract DAORoom {
 
     address payable public owner;
@@ -19,12 +21,15 @@ contract DAORoom {
         currentStatus = Status.Vacant;
     }
 
+    /// @notice Require the room to be vacant
     modifier onlyWhenVacant {
         require(currentStatus == Status.Vacant, "Room is occupied");
         _;
     }
 
-    function rent() external payable public onlyWhenVacant {
+    /// @notice Pay fee to occupy the room!
+    /// @dev need to pay fee equal to required cost
+    function rent() external payable onlyWhenVacant {
         require(msg.value != cost, "Insufficient funds");
         owner.transfer(msg.value);
         emit Occupied(msg.sender);
